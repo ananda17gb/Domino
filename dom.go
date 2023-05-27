60a7a78afdccf8ea0eae77419863943c79a2cf59
AnandaAW <ananda.widigdo17@gmail.com>
2023-05-27 19:52:40 +0700

    ngapus notes

--- dom.go
+++ dom.go
@@ -1,12 +1,3 @@
-/*
-1. Use arrays and structures when appropriate
-2. Must implement at least the sequential and binary search algorithms, deleting and modifying data stored in the array on/or structure
-3. Must implement selection sort and insertion sort algorithms, each for different data or field, and can be used to sort asc or dsc
-4. Must be structured one entry and one exit, aka no return, break, or continue in any loop
-5. Global definition is forbidden for variabel declaration, it is only for newly defined data types and/or constant definition
-
-*/
-
 package main
 
 type tileNum struct {
